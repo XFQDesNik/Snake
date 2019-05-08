959fbeef44681ce942dd4a31164aee118938f231
QDesNik <30504356+XFQDesNik@users.noreply.github.com>
2019-05-08 14:19:59 +0300

    Create Mouse.java

---
+++ Mouse.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task23.task2312;
+
+
+public class Mouse {
+    private int x;
+    private int y;
+    
+    public int getX() {
+        return this.x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
